62c49f4e3fa4c2b0b533e3005a7dd443ca5d87cd
Fabio Dos Santos <fabio2st@hotmail.com>
2022-10-17 01:57:30 -0300

    03.Notas

---
+++ 03.NotasCs/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+static class Program
+{
+    public static void Main(string[] args)
+    {
+        short cantidadAlumnos = IngresarCantidadAlumnos();
+        short cantidadNotas = IngresarCantidadNotas();
+        string[] alumnos = new string[cantidadAlumnos - 1 + 1];
+        byte[,] notas = new byte[cantidadAlumnos - 1 + 1, cantidadNotas - 1 + 1];
+        decimal[] promedios = new decimal[cantidadAlumnos - 1 + 1];
+        Process(alumnos, notas, promedios);
+        OutputMejoresPromedios(alumnos, promedios);
+    }
+
+    private static short IngresarCantidadAlumnos()
+    {
+        Int16 cantidadAlumnos;
+        do
+        {
+            Console.Write("Ingrese la cantidad de alumnos (40 máximo): ");
+            cantidadAlumnos = Convert.ToInt16( Console.ReadLine());
+        }
+        while (cantidadAlumnos > 40);
+        return cantidadAlumnos;
+    }
+    private static short IngresarCantidadNotas()
+    {
+        Int16 cantidadNotas;
+        do
+        {
+            Console.Write("Ingrese la cantidad de Notas (4 máximo): ");
+            cantidadNotas = Convert.ToInt16(Console.ReadLine());
+        }
+        while (cantidadNotas > 4);
+        return cantidadNotas;
+    }
+    private static void Process(string[] alumnos, byte[,] notas, decimal[] promedios)
+    {
+        for (var alumnoIndex = 0; alumnoIndex <= alumnos.Length - 1; alumnoIndex++)
+        {
+            alumnos[alumnoIndex] = IngresarAlumno(alumnos);
+            IngresarNotas(notas, alumnoIndex);
+            promedios[alumnoIndex] = PromediarNotas(notas, alumnoIndex);
+            OutputResultadoAlumno(promedios[alumnoIndex]);
+        }
+    }
+    private static string IngresarAlumno(string[] alumnos)
+    {
+        string nombre;
+        do
+        {
+            Console.Write("Ingrese el nombre: ");
+            nombre = Console.ReadLine();
+        }
+        while (!IsNombreValido(nombre, alumnos));
+        return nombre;
+    }
+    public static bool IsNombreValido(string nombre, string[] alumnos)
+    {
+        if (nombre.Length < 3)
+        {
+            Console.WriteLine("El nombre debe tener al menos 3 caracteres");
+            return false;
+        }
+        foreach (string item in alumnos)
+        {
+            if (nombre == item)
+            {
+                Console.WriteLine("El alumno ya existe");
+                return false;
+            }
+        }
+        return true;
+    }
+    private static void IngresarNotas(byte[,] notas, int alumnoIndex)
+    {
+        for (var notaIndex = 0; notaIndex <= notas.GetLength(1) - 1; notaIndex++)
+        {
+            short nota;
+            do
+            {
+                Console.Write($"Ingrese la {notaIndex + 1}º nota: ");
+                nota = Convert.ToInt16(Console.ReadLine());
+            }
+            while (!IsNotaValida(nota));
+            notas[alumnoIndex, notaIndex] = (byte)nota;
+        }
+    }
+    private static bool IsNotaValida(short nota)
+    {
+        if (nota >= 0 & nota <= 10)
+            return true;
+        else
+        {
+            Console.WriteLine("Nota fuera de rango!");
+            return false;
+        }
+    }
+    private static decimal PromediarNotas(byte[,] notas, int alumnoIndex)
+    {
+        decimal total = 0;
+        for (var notaIndex = 0; notaIndex <= notas.GetLength(1) - 1; notaIndex++)
+            total += notas[alumnoIndex, notaIndex];
+        return total / notas.GetLength(1);
+    }
+    private static void OutputResultadoAlumno(decimal promedio)
+    {
+        if (promedio >= 6)
+            Console.WriteLine("El alumno Aprobo con " + promedio);
+        else
+            Console.WriteLine("El alumno Desaprobo con " + promedio);
+    }
+    private static void OutputMejoresPromedios(string[] alumnos, decimal[] promedios)
+    {
+        decimal mejorNota = GetMejorNota(promedios);
+        Console.WriteLine($"Mejores alumnos con {mejorNota} de promedio");
+        for (var index = 0; index <= promedios.Length - 1; index++)
+        {
+            if (promedios[index] == mejorNota)
+                Console.WriteLine(alumnos[index]);
+        }
+    }
+    private static decimal GetMejorNota(decimal[] promedios)
+    {
+        decimal mejorNota = 0;
+        foreach (decimal promedio in promedios)
+        {
+            if (promedio > mejorNota)
+                mejorNota = promedio;
+        }
+        return mejorNota;
+    }
+}
---
+++ 03.NotasVb/Program.vb
@@ -0,0 +1,109 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim cantidadAlumnos As Short = IngresarCantidadAlumnos()
+        Dim cantidadNotas As Short = IngresarCantidadNotas()
+        Dim alumnos(cantidadAlumnos - 1) As String
+        Dim notas(cantidadAlumnos - 1, cantidadNotas - 1) As Byte
+        Dim promedios(cantidadAlumnos - 1) As Decimal
+        Process(alumnos, notas, promedios)
+        OutputMejoresPromedios(alumnos, promedios)
+    End Sub
+
+    Private Function IngresarCantidadAlumnos() As Short
+        Dim cantidadAlumnos As Int16
+        Do
+            Console.Write("Ingrese la cantidad de alumnos (40 máximo): ")
+            cantidadAlumnos = Console.ReadLine()
+        Loop Until cantidadAlumnos <= 40
+        Return cantidadAlumnos
+    End Function
+    Private Function IngresarCantidadNotas() As Short
+        Dim cantidadNotas As Int16
+        Do
+            Console.Write("Ingrese la cantidad de Notas (4 máximo): ")
+            cantidadNotas = Console.ReadLine()
+        Loop Until cantidadNotas <= 4
+        Return cantidadNotas
+    End Function
+    Private Sub Process(alumnos() As String, notas(,) As Byte, promedios() As Decimal)
+        For alumnoIndex = 0 To alumnos.Length - 1
+            alumnos(alumnoIndex) = IngresarAlumno(alumnos)
+            IngresarNotas(notas, alumnoIndex)
+            promedios(alumnoIndex) = PromediarNotas(notas, alumnoIndex)
+            OutputResultadoAlumno(promedios(alumnoIndex))
+        Next
+    End Sub
+    Private Function IngresarAlumno(alumnos() As String) As String
+        Dim nombre As String
+        Do
+            Console.Write("Ingrese el nombre: ")
+            nombre = Console.ReadLine()
+        Loop Until IsNombreValido(nombre, alumnos)
+        Return nombre
+    End Function
+    Function IsNombreValido(nombre As String, alumnos() As String) As Boolean
+        If nombre.Length < 3 Then
+            Console.WriteLine("El nombre debe tener al menos 3 caracteres")
+            Return False
+        End If
+        For Each item As String In alumnos
+            If nombre = item Then
+                Console.WriteLine("El alumno ya existe")
+                Return False
+            End If
+        Next
+        Return True
+    End Function
+    Private Sub IngresarNotas(notas(,) As Byte, alumnoIndex As Integer)
+        For notaIndex = 0 To notas.GetLength(1) - 1
+            Dim nota As Short
+            Do
+                Console.Write($"Ingrese la {notaIndex + 1}º nota: ")
+                nota = Console.ReadLine()
+            Loop Until IsNotaValida(nota)
+            notas(alumnoIndex, notaIndex) = nota
+        Next
+    End Sub
+    Private Function IsNotaValida(nota As Short) As Boolean
+        If nota >= 0 And nota <= 10 Then
+            Return True
+        Else
+            Console.WriteLine("Nota fuera de rango!")
+            Return False
+        End If
+    End Function
+    Private Function PromediarNotas(notas(,) As Byte, alumnoIndex As Integer) As Decimal
+        Dim total As Decimal
+        For notaIndex = 0 To notas.GetLength(1) - 1
+            total += notas(alumnoIndex, notaIndex)
+        Next
+        Return total / notas.GetLength(1)
+    End Function
+    Private Sub OutputResultadoAlumno(promedio As Decimal)
+        If promedio >= 6 Then
+            Console.WriteLine("El alumno Aprobo con " & promedio)
+        Else
+            Console.WriteLine("El alumno Desaprobo con " & promedio)
+        End If
+    End Sub
+    Private Sub OutputMejoresPromedios(alumnos() As String, promedios() As Decimal)
+        Dim mejorNota As Decimal = GetMejorNota(promedios)
+        Console.WriteLine($"Mejores alumnos con {mejorNota} de promedio")
+        For index = 0 To promedios.Length - 1
+            If promedios(index) = mejorNota Then
+                Console.WriteLine(alumnos(index))
+            End If
+        Next
+    End Sub
+    Private Function GetMejorNota(promedios() As Decimal) As Decimal
+        Dim mejorNota As Decimal
+        For Each promedio As Decimal In promedios
+            If promedio > mejorNota Then
+                mejorNota = promedio
+            End If
+        Next
+        Return mejorNota
+    End Function
+End Module
