caa9628a7d82aecf6aa3cb0a51add1349991125f
Fabio Dos Santos <fabio2st@hotmail.com>
2022-10-17 00:17:09 -0300

    02.Ventas

---
+++ 02.VentasCs/Program.cs
@@ -0,0 +1,63 @@
+﻿static class Ventas
+{
+    public static void Main(string[] args)
+    {
+        ushort[] codigos = new ushort[] { 1, 2, 5 };
+        string[] nombres = new string[] { "papa", "manzana", "uva" };
+        decimal[] precios = new decimal[] { 2.6M, 1.2M, 4.5M };
+        Process(codigos, nombres, precios);
+    }
+    private static void Process(ushort[] codigos, string[] nombres, decimal[] precios)
+    {
+        ushort codigo;
+        decimal total = 0;
+        codigo = LeerCodigo();
+        while (codigo != 0)
+        {
+            if (isCodigoExiste(codigo, codigos))
+            {
+                total += ProcessProduct(codigos, nombres, precios, codigo);
+                Console.WriteLine("Total: " + total);
+            }
+            else
+                Console.WriteLine("Código no existe");
+            codigo = LeerCodigo();
+        }
+    }
+    private static decimal ProcessProduct(ushort[] codigos, string[] nombres, decimal[] precios, ushort codigo)
+    {
+        ushort index;
+        decimal cantidad, subtotal;
+        index = GetIndex(codigo, codigos);
+        Console.WriteLine("Producto: " + nombres[index]);
+        Console.WriteLine("Precio: " + precios[index]);
+        Console.Write("Ingrese cantidad: ");
+        cantidad = Convert.ToDecimal( Console.ReadLine());
+        subtotal = precios[index] * cantidad;
+        Console.WriteLine("SubTotal: " + subtotal);
+        return subtotal;
+    }
+    public static ushort LeerCodigo()
+    {
+        Console.Write("Ingrese un codigo de producto: ");
+        return Convert.ToUInt16( Console.ReadLine());
+    }
+    public static bool isCodigoExiste(ushort codigo, ushort[] codigos)
+    {
+        foreach (var valor in codigos)
+        {
+            if (valor == codigo)
+                return true;
+        }
+        return false;
+    }
+    public static ushort GetIndex(ushort codigo, ushort[] codigos)
+    {
+        for (ushort x = 0; x <= codigos.Length - 1; x++)
+        {
+            if (codigo == codigos[x])
+                return x;
+        }
+        return default(ushort);
+    }
+}
---
+++ 02.VentasVb/Program.vb
@@ -0,0 +1,56 @@
+﻿Imports System
+
+Module Ventas
+    Sub Main(args As String())
+        Dim codigos = New UShort() {1, 2, 5}
+        Dim nombres = New String() {"papa", "manzana", "uva"}
+        Dim precios = New Decimal() {2.6, 1.2, 4.5}
+        Process(codigos, nombres, precios)
+    End Sub
+    Private Sub Process(codigos() As UShort, nombres() As String, precios() As Decimal)
+        Dim codigo As UShort
+        Dim total As Decimal
+        codigo = LeerCodigo()
+        Do Until codigo = 0
+            If isCodigoExiste(codigo, codigos) Then
+                total += ProcessProduct(codigos, nombres, precios, codigo)
+                Console.WriteLine("Total: " & total)
+            Else
+                Console.WriteLine("Código no existe")
+            End If
+            codigo = LeerCodigo()
+        Loop
+    End Sub
+    Private Function ProcessProduct(codigos() As UShort, nombres() As String, precios() As Decimal, codigo As UShort) As Decimal
+        Dim index As UShort
+        Dim cantidad, subtotal As Decimal
+        index = GetIndex(codigo, codigos)
+        Console.WriteLine("Producto: " & nombres(index))
+        Console.WriteLine("Precio: " & precios(index))
+        Console.Write("Ingrese cantidad: ")
+        cantidad = Console.ReadLine()
+        subtotal = precios(index) * cantidad
+        Console.WriteLine("SubTotal: " & subtotal)
+        Return subtotal
+    End Function
+    Function LeerCodigo() As UShort
+        Console.Write("Ingrese un codigo de producto: ")
+        Return Console.ReadLine()
+    End Function
+    Function isCodigoExiste(codigo As UShort, codigos() As UShort) As Boolean
+        For Each valor In codigos
+            If valor = codigo Then
+                Return True
+            End If
+        Next
+        Return False
+    End Function
+    Function GetIndex(codigo As UShort, codigos() As UShort) As UShort
+        For x = 0 To codigos.Length - 1
+            If codigo = codigos(x) Then
+                Return x
+            End If
+        Next
+        Return Nothing
+    End Function
+End Module
